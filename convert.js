96a2269ee4a57d1c78623f8a51ada26cbcd58f44
mahfuzarrahmanmunna <mdmahfuzarrahmanmunna44@gmail.com>
2025-02-14 23:12:47 +0600

    js task 1 is already done

---
+++ convert.js
@@ -0,0 +1,6 @@
+function inchToFeet(inch) {
+    const feet = inch / 12;
+    return feet
+}
+const myHeight = inchToFeet(80)
+console.log(myHeight)
---
+++ js-task1.js
@@ -0,0 +1,6 @@
+function celsiusToFahrenheit(celsius) {
+    let convert = ((celsius * (9 / 5)) + 32);
+    return convert
+}
+let first = celsiusToFahrenheit(3)
+console.log(first)
